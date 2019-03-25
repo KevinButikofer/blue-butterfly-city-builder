5e6df9afc775f9fded3c8e756541e1a92ada23d0
jules.perrelet1 <jules.perrelet@he-arc.ch>
2019-03-25 11:25:54 +0100

    add building selection ui

---
+++ Assets/Scripts/imageBuildingClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class imageBuildingClick : MonoBehaviour, IPointerClickHandler
+{
+
+    public Canvas canvasBuildings;
+    public int buildingIndex;
+    public Image imageCurrentBuilding;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        canvasBuildings.gameObject.SetActive(false);
+        imageCurrentBuilding.sprite = this.GetComponent<Image>().sprite;
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
